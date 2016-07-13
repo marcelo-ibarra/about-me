61f1f3932466f17ed0fa49ff25862358615baf81
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-12 19:42:57 -0600

    Added a footer to the about-me project

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 		<h2>This is a great experience for me; I'm here doing it. I really want to be able to learn this!</h2>
 		<a href="https:// go to el bosque.org" target="_blank">Go to el bosque.org</a>
 	</body>
-	<footer>2016</footer>
+	<footer>& copy 2016</footer>
 	</html>
 
