d19f98311ff81235264845098564a1c78cb8498d
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-12 18:20:40 -0600

    Added title to abou-me project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="urf-8/>"
+		<title>Here I go</title>
+	</head>
+	<body>
+		<h1>This is Awesome, Marcelo Ibarra!</h1>
+		<h2>This is a great experience for me; I'm here doing it</h2>
+	</body>
+	</html>
+
