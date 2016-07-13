c00c4f353c3919a13995a5186faca0f8c3f0929a
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-12 19:23:41 -0600

    Added a footer to the about-me project

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 	</head>
 	<body>
 		<h1>This is Awesome, Marcelo Ibarra!</h1>
-		<h2>This is a great experience for me; I'm here doing it. I really want to be able to learn to this!</h2>
-		<a href="https://go to catipsum.com" target="_blank">Go to catipsum.com</a>
+		<h2>This is a great experience for me; I'm here doing it. I really want to be able to learn this!</h2>
+		<a href="https:// go to el bosque.org" target="_blank">Go to el bosque.org</a>
 	</body>
+	<footer>2016</footer>
 	</html>
 
