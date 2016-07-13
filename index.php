cfccf2063c479ad46b994ca9957c8cdc608ae1de
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-13 08:32:36 -0600

    Corrected the link on abou-me project

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 		<title>Here I go</title>
 	</head>
 	<body>
-		<h1>This is Awesome, Marcelo Ibarra!</h1>
+		<h1>This is Awesome! My name is Marcelo Ibarra!</h1>
 		<h2>This is a great experience for me; I'm here doing it. I really want to be able to learn this!</h2>
-		<a href="https:// go to el bosque.org" target="_blank">Go to el bosque.org</a>
+		<a href="https://oldcars.com" target="_blank">Go to this awesome wesite oldcars.com!</a>
 	</body>
-	<footer>& copy 2016</footer>
+	<footer>&copy; 2016</footer>
 	</html>
 
