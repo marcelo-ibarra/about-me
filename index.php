a05cbb8ea7200fb43018a8a9002545701dc0d100
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-13 20:04:56 -0600

    Added paragraph to the about-me project

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 	<html lang="en">
 	<head>
 		<meta charset="urf-8/>"
-		<title>Here I go</title>
+		<title>Marcelo Ibarra</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 	</head>
 	<body>
-		<h1>This is Awesome! My name is Marcelo Ibarra!</h1>
-		<h2>This is a great experience for me; I'm here doing it. I really want to be able to learn this!</h2>
+		<h1>Front End Developer!</h1>
+		<h2>Fiancial and Warehouse Management background</h2>
+		<h3>I'm a hardworking individual who likes challenge and enjoys learning new things. A very strong team player who's dedicated and everall responsible. Depending on the length and detail of the content here, the Contact Form may also be incorporated here.</h3>
 		<a href="https://oldcars.com" target="_blank">Go to this awesome wesite oldcars.com!</a>
+		<img class="car-image" src="images/76_Chevrolet_Monte_Carlo.jpg" alt="">
 	</body>
 	<footer>&copy; 2016</footer>
 	</html>
