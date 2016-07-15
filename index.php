6e00df5164a471ab9d2bee65b6b0aefe7a0220e0
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-15 10:29:04 -0600

    Added more information to paragraph

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 	</head>
 	<body>
-		<h1>Front End Developer!</h1>
-		<h2>Fiancial and Warehouse Management background</h2>
-		<h3>I'm a hardworking individual who likes challenge and enjoys learning new things. A very strong team player who's dedicated and overall responsible. With over 15 years of leading teams and providing great customer service. Depending on the length and detail of the content here, the Contact Form may also be incorporated here.</h3>
+		<h1>Enjoy the outdoors and spending time with family!</h1>
+		<h2>Really want to be able to learn how to code properly and quickly</h2>
+		<h3>I'm a hardworking individual who likes challenge and enjoys learning new things. Very dedicated to my family and enjoy my dog as well; her name is Tahzzie. She a 10 Month Pitbull</h3>
 		<a href="https://oldcars.com" target="_blank">oldcars.com!</a>
-		<img class="car-image" src="images/76_Chevrolet_Monte_Carlo.jpg" alt="center">
+		<img class="car-image" src="images/76_Chevrolet_Monte_Carlo.jpg" alt=>
 	</body>
 	<footer>&copy; 2016</footer>
 	</html>
